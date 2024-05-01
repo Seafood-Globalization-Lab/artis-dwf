7f20f5f150d4fc5a6a4fe1884004882973795d23
theamarks <theamarks@gmail.com>
2024-05-01 10:40:49 -0700

    Add object definitions in quarto doc not external

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -2,28 +2,192 @@
 
 # loop through list of countries, knit the Rmd/Quarto file for each country, and save the output as individual HTML reports.
 
-# Load Packages
+
+# Load Packages & Create variables -----------------------------------------
 library(quarto) # Rmd successor
+library(tidyverse)
+library(countrycode)
+library(exploreARTIS)
 
-# List of Oceana countries of interest: 
-countries <- c("Belize", "Brazil", "Canada", "Chile", "Mexico", "Philippines", "Peru", "UK", "USA", "Spain", "Malaysia", "Ghana", "Senegal")
+# # List of Oceana countries of interest: 
+# countries <- c("Belize", "Brazil", "Canada", "Chile", "Mexico", "Philippines", "Peru", "UK", "USA", "Spain", "Malaysia", "Ghana", "Senegal")
+# 
+# # Standardize country names 
+# countries_std <- countrycode(countries,
+#                               origin = "country.name",
+#                               destination = "country.name")
+# 
+# # set year for analysis
+# year_int <- 2019
 
-# Standardize country names 
-countries_std <- countrycode(countries,
-                              origin = "country.name",
-                              destination = "country.name")
+# Load Data & Scripts -------------------------------------------------
+#existing SAU data in ARTIS
+artis_sau <- read.csv("data/SAU_ARTIS_2010-2020.csv")
 
-# set year for analysis
-year_int <- 2019
+# load new SAU data - AM from ARTIS repo ./QA/outputs/
+# Only contains species & countries standardized SAU marine capture (not EEZ) 
+prod_sau <- read_csv("./data/standardized_sau_prod.csv")
 
-# Loop through each country to generate individual reports
-for (a_country in countries_std) {
+source(file.path("./scripts/standardize_sau.R"))
+
+# Clean Data -------------------------------------------------
+prod_sau <- prod_sau %>%
   
-  # Knit the Rmd/Quarto file
-  quarto_render(
-    input = file.path("country_profile_template.Qmd"),  
-    output_file = file.path("./output", paste0("report_", a_country))
-  )
+  # clarify column names
+  rename(prod_iso3 = country_iso3_alpha,
+         catch_eez = eez) %>% 
   
-  cat(paste("Report for", a_country, "generated.\n"))
-}
+  # Break apart catch_eez column - identify ISO3 codes with one of the 3 eez columns
+  separate(catch_eez, 
+           into = c("catch_eez_1", "catch_eez_2"), 
+           sep = "\\(", remove = FALSE) %>%
+  mutate(catch_eez_2 = gsub("\\)", "", catch_eez_2)) %>%
+  
+  # create new cleaned eez column - eez_iso3 - from countrycode library names
+  mutate(catch_eez_iso3c = countrycode(catch_eez, 
+                                       origin = "country.name", 
+                                       destination = "iso3c")) %>% 
+  mutate(catch_eez_iso3c = case_when(
+    (!is.na(catch_eez_iso3c)) ~ catch_eez_iso3c, 
+    (is.na(catch_eez_iso3c) ~ countrycode(catch_eez_1, 
+                                          origin = "country.name", 
+                                          destination = "iso3c")))) %>%
+  mutate(catch_eez_iso3c = case_when(
+    (!is.na(catch_eez_iso3c)) ~ catch_eez_iso3c, 
+    (is.na(catch_eez_iso3c) ~ countrycode(catch_eez_2, 
+                                          origin = "country.name", 
+                                          destination = "iso3c")))) %>%
+  
+  # correct single instance 
+  mutate(catch_eez_iso3c = case_when(
+    (catch_eez == "US Virgin Isl.") ~ "USA",
+    TRUE ~ catch_eez_iso3c
+  )) %>%
+  
+  # use cleaned ISO3 codes to generate standard country name column
+  mutate(catch_eez_name = countrycode(catch_eez_iso3c, 
+                                      origin = "iso3c", 
+                                      destination = "country.name")) %>% 
+  
+  # adds artis_iso3 and catch_artis_country_name columns
+  standardize_sau_eez("catch_eez_iso3c", "catch_eez_name") %>% 
+  rename(catch_artis_iso3 = artis_iso3,
+         catch_artis_country_name = artis_country_name) %>% 
+  
+  # remove columns only used for standardization process
+  select(-catch_eez_1, -catch_eez_2, -catch_eez) %>% 
+  
+  # aggregate catch quantity records
+  # group_by(group_by(across(-quantity))) %>% 
+  # summarize(live_weight_t = sum(quantity)) %>% 
+  group_by(year, prod_iso3, SciName, prod_method, 
+           habitat, catch_artis_iso3, catch_artis_country_name) %>% 
+  summarise(live_weight_t = sum(quantity)) %>% 
+  ungroup() %>% 
+  
+  # Tag domestic versus foreign fishing
+  mutate(dwf = case_when(
+    (catch_artis_iso3 == prod_iso3) ~ "domestic",
+    TRUE ~ "foreign"
+  ))
+
+# Summary Stats & Figures ----------------------------------------------
+
+# Landings x Year x (domestic vs foreign)
+prod_sau %>%
+  group_by(year, dwf) %>% 
+  summarise(live_weight_t = sum(live_weight_t)) %>%
+  ggplot(aes(x = year, y = live_weight_t/1000000, fill = dwf)) +
+  geom_area() +
+  labs(x = "", 
+       y = "Landings (mil t, live weight)",
+       title = "Overall Domestic vs Foreign Landings") +
+  theme_bw()
+
+# Average Landings x Country
+prod_sau %>%
+  filter(dwf == "foreign") %>%
+  group_by(catch_artis_country_name) %>%
+  summarise(total_dwf = sum(live_weight_t)) %>%
+  filter(total_dwf > 1000000) %>%
+  ungroup() %>%
+  ggplot(aes(y = fct_reorder(catch_artis_country_name, total_dwf), 
+             x = total_dwf/(1000000*length(unique(prod_sau$year))))) +
+  geom_bar(stat = "identity") +
+  labs(y = "", 
+       x = "Ave. Landings (mil t, live weight)",
+       title = "") +
+  theme_bw()
+
+# Top DWF fishing countries x Foreign landings x year
+prod_sau %>%
+  filter(dwf == "foreign") %>%
+  group_by(catch_artis_country_name) %>%
+  mutate(total_dwf = sum(live_weight_t)) %>%
+  filter(total_dwf > 15000000) %>%
+  group_by(year, catch_artis_country_name) %>% 
+  summarise(live_weight_t = sum(live_weight_t)) %>%
+  ggplot(aes(x = year, y = live_weight_t/1000000, fill = catch_artis_country_name)) +
+  geom_area() +
+  labs(x = "", 
+       y = "Landings (mil t, live weight)", 
+       fill = "Fishing entity",
+       title = "Top countries landings from distant water fishing") +
+  theme_bw()
+
+# Top species caught by DFW
+prod_sau %>%
+  filter(dwf == "foreign") %>%
+  group_by(SciName) %>%
+  mutate(total_dwf = sum(live_weight_t)) %>%
+  filter(total_dwf > 5000000) %>%
+  group_by(year, SciName) %>% 
+  summarise(live_weight_t = sum(live_weight_t)) %>%
+  ggplot(aes(x = year, y = live_weight_t/1000000, fill = SciName)) +
+  geom_area() +
+  labs(x = "", 
+       y = "Landings (mil t, live weight)", 
+       fill = "",
+       title = "Top species landings by distant water fishing") +
+  theme_bw()
+
+# number of observations
+prod_sau %>%
+  nrow()
+
+# number of low production observations
+prod_sau %>%
+  filter(live_weight_t < 0.1) %>%
+  nrow()
+
+# Join Prod SAU & ARTIS SAU data ---------------------------------------------
+
+# Proportion of landings by country flag captured in recorded source eezs
+prod_sau_props <- prod_sau %>%
+  # aggregate landings amount - 
+  # disregard habitat, production method, sector, end use
+  group_by(year, prod_iso3, SciName, 
+           catch_artis_country_name, catch_artis_iso3, dwf) %>%
+  summarise(live_weight_t = sum(live_weight_t)) %>% 
+  # calculate prop catch over each source eez - 
+  # does not contract df over 2nd group_by()
+  group_by(year, prod_iso3, SciName) %>% # needs to be exactly what data is joining by after
+  mutate(prop_by_catch_eez = live_weight_t/sum(live_weight_t)) %>%
+  select(-live_weight_t)
+
+# Disaggregate ARTIS by EEZ of catch - join datasets
+artis_eez <- artis_sau %>% 
+  # prod_sau (therefor prod_sau_props) is inherently only marine capture - match artis_sau data
+  filter(habitat == "marine", 
+         method == "capture") %>%
+  # pull prod_sau_props data for year year, source country, and species
+  left_join(prod_sau_props, 
+            by = c("year", 
+                   "source_country_iso3c" = "prod_iso3", 
+                   "sciname" = "SciName")) %>%
+  # recalculate live_weight_t catch - each trade and product record gets split apart by the number of catch eez from prod_sau_props - essentially assigning a probability a product was caught in a specific eez. 
+  mutate(live_weight_t = live_weight_t*prop_by_catch_eez)
+# many-to-many warning is what we expect here - one row of artis_sau correlates with multiple prod_sau eez
+
+
+
