b97b4c573fe13c4cb1a33609ccc49b0a8427c685
theamarks <theamarks@gmail.com>
2024-05-21 16:44:56 -0700

    read in local consumption table. Add to parameters passed to Rmd render

--- run-analysis-report.R
+++ run-analysis-report.R
@@ -10,6 +10,7 @@ library(exploreARTIS)
 library(rmarkdown)
 library(readr)
 library(tidyr)
+library(sqldf)
 
 
 # Create directories ------------------------------------------------------
@@ -29,6 +30,9 @@ prod_sau <- read_csv(
   file.path(".", "data", "standardized_sau_prod.csv", 
             fsep = "/"))
 
+# Read in local SAU consumption file (eventually replace with Heroku db)
+consumption <- read_csv(
+  file.path(".", "data", "complete_consumption.csv"))
 
 # Run Scripts -------------------------------------------------------------
 
@@ -37,26 +41,25 @@ source(file.path(".", "scripts", "functions.R",
                  fsep = "/"))
 
 # pull consumption data from Heroku server database
-source(file.path(".", "scripts", "load_db_data.R"))
+#source(file.path(".", "scripts", "load_db_data.R"))
 
 # 1) standardize production SAU data
 # 2) proportion of landings by producer captured in recorded source EEZs
 # 3) disaggregate ARTIS by EEZ of catch - Join production SAU and ARTIS SAU data 
 source(file.path(".", "scripts", "clean_data.R"))
 
-
 # Countries of interest ---------------------------------------------------
 
 # Vector of Oceana countries:
 # countries <- c("Belize", "Brazil", "Canada", "Chile", "Mexico", "Philippines", "Peru", "UK", "USA", "Spain", "Malaysia", "Ghana", "Senegal")
 countries <- c("Belize")
-year_int <- c(2016, 2017, 2018, 2019, 2020)
+#year_int <- c(2016, 2017, 2018, 2019, 2020)
 
 # Standardize country names
 countries_std <- countrycode(countries,
                               origin = "country.name",
                               destination = "iso3c")
-
+# for testing only
 countries_i <- countries_std
 
 # Build DWF profiles ---------------------------------------------
@@ -72,6 +75,11 @@ for (i in 1:length(countries_std)) {
     filter(habitat == "marine", 
            method == "capture", 
            year %in% year_int, 
+           source_country_iso3c == countries_i)
+  
+  # filter consumption table by focal country 
+  consumption_i <- consumption %>% 
+    filter(year %in% unique(artis_eez$year),
            source_country_iso3c == countries_i)
   
   rmarkdown::render(
