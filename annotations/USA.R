a1b268dc4020753121995e4cebb8b7a771033106
theamarks <theamarks@gmail.com>
2024-07-12 22:06:21 -0700

    Add annotation folder and country specific files

---
+++ annotations/USA.R
@@ -0,0 +1,45 @@
+## ---- 1-fig-prct-dwf --------
+
+cat("This is the **annotation** for figure 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+
+## ---- 2-fig-line-dwf-eez --------
+
+cat("Annotation **Figure 2**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+
+## ---- 3-fig-bar-dwf-species --------
+
+cat("Annotation **Figure 3**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 4-fig-stacked-consum-dwf -------
+
+cat("Annotation **Figure 4**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 1-table-dwf-species -------
+
+cat("Annotation **Table 1**")
+
+## ---- 5-10-figs-dwf-species-sankeys -------
+
+cat("Annotations **Figures 5-10**. Not exactly sure how to insert annotations for each sankey figure. Possibly inside of `purrr::map()`")
+
+## ---- 11-fig-consump -------
+
+cat("Annotation **Figure 11**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 12-fig-facet-consump -------
+
+cat("Annotation **Figure 12**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 13-fig-bar-top-sp-consump -------
+
+cat("Annotation **Figure 13**. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 2-table-top-sp-consumed -------
+
+cat("**Table 2** annotations. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+## ---- 14-19-figs-sankey-consump -------
+
+cat("Annotation **Figure 14-19**. Not exactly sure how to insert annotations for each sankey figure. Possibly inside of `purrr::map()` OR generate & save figures in folder and `knitr::include_graphics()` to place figures back into Rmd")
