a2b626087ca8ce051db3811c7e83850a64af0939
theamarks <theamarks@gmail.com>
2024-07-12 22:05:18 -0700

    add if exists statements to reading in large data files. Add manual corrections for NA values found in common_name from sciname join. Moved sciname_metadata join to run-analysis-report.R script.

--- run-analysis-report.R
+++ run-analysis-report.R
@@ -14,6 +14,7 @@ library(sqldf)
 library(glue)
 library(cleanrmd)
 library(tufte)
+library(stringr)
 
 
 # Create directories ------------------------------------------------------
@@ -30,17 +31,39 @@ anndir <- file.path(".", "annotations", fsep = "/")
 
 # load new SAU data - AM from ARTIS repo ./QA/outputs/
 # Only contains species & countries standardized SAU marine capture (not EEZ) 
-prod_sau <- read_csv(
-  file.path(".", "data", "standardized_sau_prod.csv",
-            fsep = "/"))
+if(exists("prod_sau") == FALSE){
+  prod_sau <- read_csv(
+    file.path(".", "data", "standardized_sau_prod.csv",
+              fsep = "/"))
+}
 
 # Read in local SAU consumption file (eventually replace with Heroku db)
-consumption <- read_csv(
-  file.path(".", "data", "complete_consumption.csv"))
+if(exists("consumption") == FALSE){
+  consumption <- read_csv(
+    file.path(".", "data", "complete_consumption.csv")) 
+}
 
+# read in scientific name metadata (contains common names)
+# sciname_metadata <- read_csv(
+#     file.path(".", "data", "sciname_metadata.csv"))
 sciname_metadata <- read_csv(
-  file.path(".", "data", "sciname_metadata.csv")
-)
+  file.path(".", "data", "sciname_metadata_heroku.csv"))
+
+
+# Clean sciname_metadata --------------------------------------------------
+# correct NAs found in sciname_metadata that are used in this analysis
+sciname_common <- sciname_metadata %>%  
+  select(sciname, common_name) %>% 
+  add_row(sciname = "merluccius gayi", 
+          common_name = "pacific hake") %>% # found in fb_common_to_sci
+  add_row(sciname = "pentheroscion mbizi", 
+          common_name = "blackmouth croaker") %>% # found in fb_common_to_sci
+  add_row(sciname = "paralichthyidae", 
+          common_name = "large-tooth flounder") %>% #fishbase website family
+  # replace common names with "=" with shortened version
+  mutate(common_name = if_else(str_detect(common_name, "="), 
+                               str_extract(common_name, "(?<=\\=)[^\\)]+"), 
+                               common_name))
 
 message("data import is complete")
 
@@ -50,14 +73,17 @@ message("data import is complete")
 source(file.path(".", "scripts", "functions.R", 
                  fsep = "/"))
 
-# pull consumption data from Heroku server database
-#source(file.path(".", "scripts", "load_db_data.R"))
+# FIXIT: pull consumption data from Heroku server database
+# source(file.path(".", "scripts", "load_db_data.R"))
 
 # 1) standardize production SAU data
 # 2) proportion of landings by producer captured in recorded source EEZs
 # 3) disaggregate consumption by EEZ of catch - 
 # Join production SAU and ARTIS SAU data 
-source(file.path(".", "scripts", "clean_data.R"))
+if(exists("consumption_eez") == FALSE){
+  source(file.path(".", "scripts", "clean_data.R"))
+}
+
 
 message("running clean_data.R is complete")
 
@@ -66,18 +92,16 @@ message("running clean_data.R is complete")
 # Vector of Oceana countries:
 countries <- c("Belize", "Brazil", "Canada", "Chile", "Mexico", "Philippines", "Peru", "UK", "USA", "Spain", "Malaysia", "Ghana", "Senegal")
 #countries <- c("Belize")
-#countries <- c("Philippines")
+#countries <- c("Belize", "Brazil", "Canada")
+#countries <- "Chile"
 
 # Standardize country names
 countries_std <- countrycode(countries,
                               origin = "country.name",
                               destination = "iso3c")
 # for testing only
-countries_i <- countries_std
-
-# limit sciname metadata -------------
-sciname_metadata <- sciname_metadata %>% 
-  select(sciname, common_name)
+#countries_i <- countries_std
+  
 
 # Filter last 5 years of data ----------------------------
 max_year <- max(consumption_eez$year)
@@ -85,10 +109,13 @@ last_x_yrs <- seq(max_year - 4, max_year, by = 1)
 
 consumption_eez_2 <- consumption_eez %>% 
   filter(year %in% last_x_yrs) %>% 
-  # rename AM's cleaning data script column names
+  # rename AM's cleaning data script column names that are confusing to align with standard ARTIS column names (clean_data.R)
   rename(producer_iso3c = source_country_iso3c,
          eez_iso3c = catch_artis_iso3,
          eez_name = catch_artis_country_name)
+
+# create empty csv for recording common name NAs recorded in the Rmd
+#file.create(file.path(outdir, "common_name_na.csv"))
 
 # Build DWF profiles ---------------------------------------------
 
@@ -96,30 +123,35 @@ consumption_eez_2 <- consumption_eez %>%
 for (i in 1:length(countries_std)) {
   countries_i <- countries_std[i]
 
-  message(glue::glue("started rendering {countries_i} profile"))
+  message(glue::glue("started filtering {countries_i} data"))
   
   # 1) focal country DWF activities
-    country_i_dwf <- consumption_eez_2 %>% 
-      filter(producer_iso3c == countries_i)
+  country_i_dwf <- consumption_eez_2 %>% 
+    filter(producer_iso3c == countries_i) %>% 
+    left_join(sciname_common, by = "sciname")
 
   # 2) focal country consumption of DWF catch
-    country_i_consump <- consumption_eez_2 %>% 
-      filter(consumer_iso3c == countries_i)
+  country_i_consump <- consumption_eez_2 %>% 
+    filter(consumer_iso3c == countries_i) %>% 
+    left_join(sciname_common, by = "sciname")
     
   # 3) Fishing activity in focal country EEZ
-    country_i_eez_fishing <- consumption_eez_2 %>% 
-      filter(eez_iso3c == countries_i)
+  country_i_eez_fishing <- consumption_eez_2 %>% 
+    filter(eez_iso3c == countries_i) %>% 
+    left_join(sciname_common, by = "sciname")
 
+  message(glue::glue("started rendering {countries_i} profile"))
+    
   rmarkdown::render(
     input = "country_profile_template.Rmd",
     params = list(countries_i = countries_i,
                   country_i_dwf = country_i_dwf,
                   country_i_consump = country_i_consump,
-                  country_i_eez_fishing = country_i_eez_fishing,
-                  sciname_metadata = sciname_metadata), 
+                  country_i_eez_fishing = country_i_eez_fishing), 
     output_dir = outdir,
     output_file = paste("dwf", countries_i, "profile.pdf", sep = "_")
   )
+  
 }
 
 
